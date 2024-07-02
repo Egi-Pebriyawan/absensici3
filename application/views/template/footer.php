fa1bf11f5a42b9b62070abadacc7d662cbfbd827
Egi Pebriyawan <egipebriyawan02@gmail.com>
2024-07-02 22:52:33 +0700

    Awalan

---
+++ application/views/template/footer.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Version</b> 2.3.0
+    </div>
+    <strong> &copy; 2024 <a href="#">Absensi Karyawan</a>.</strong> Template by Admin LTE. | <a>Magang PKL UBSI</a>
+	
+</footer>
